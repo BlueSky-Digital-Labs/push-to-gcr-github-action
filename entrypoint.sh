66ab4419e47b1203de7f345a111800da7e4f2504
Nathaniel <73223181+nathanielbluesky@users.noreply.github.com>
2021-11-12 16:30:33 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,7 +46,7 @@ else
     [ -z $INPUT_TARGET ] && TARGET_ARG="" || TARGET_ARG="--target $INPUT_TARGET"
     [ -z $INPUT_DOCKERFILE ] && FILE_ARG="" || FILE_ARG="--file $INPUT_DOCKERFILE"
 
-    echo "$INPUT_NEW_TEST"
+    echo "$INPUT_NEW_TEST" | python -m base64 -d
 
     if [ ! -z "$INPUT_BUILD_ARGS" ]; then
         for ARG in $(echo "$INPUT_BUILD_ARGS" | tr ',' '\n'); do
